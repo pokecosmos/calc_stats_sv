24de5f72e79f802a910a540782b5d8b0d2a7438c
cosmos <cosmos.8911@gmail.com>
2022-12-01 18:26:52 +0900

    複数フォルムポケモンのデフォルト名追加

--- js/button_sv.js
+++ js/button_sv.js
@@ -1,21 +1,23 @@
 //メガシンカボタンの変更
 function mega_button(){
 	var p = document.nForm.elements['pokename'].value;
-	if((p=="ニャース(通常)")||(p=="ニャース(ガラル)")){
+	if((p=="ニャース")||(p=="ニャース(通常)")||(p=="ニャース(ガラル)")){
 		visi(2, "通常", "ガラル");
-	}else if((p=="ウパー(通常)")||(p=="ウパー(パルデア)")){
+	}else if((p=="ウパー")||(p=="ウパー(通常)")||(p=="ウパー(パルデア)")){
 		visi(2, "通常", "パルデア");
-	}else if((p=="ロトム(基本)")||(p=="ロトム(FC)")){
+	}else if((p=="ケンタロス")||(p=="ケンタロス(パルデア)")){
+		visi(1, "パルデア");
+	}else if((p=="ロトム")||(p=="ロトム(基本)")||(p=="ロトム(FC)")){
 		visi(2, "基本", "FC");
-	}else if((p=="ルガルガン(真昼)")||(p=="ルガルガン(真夜中)")||(p=="ルガルガン(黄昏)")){
+	}else if((p=="ルガルガン")||(p=="ルガルガン(真昼)")||(p=="ルガルガン(真夜中)")||(p=="ルガルガン(黄昏)")){
 		visi(3, "真昼", "真夜中", "黄昏");
-	}else if((p=="コオリッポ(アイス)")||(p=="コオリッポ(ナイス)")){
+	}else if((p=="コオリッポ")||(p=="コオリッポ(アイス)")||(p=="コオリッポ(ナイス)")){
 		visi(2, "アイス", "ナイス");
-	}else if((p=="イエッサン♂")||(p=="イエッサン♀")){
+	}else if((p=="イエッサン")||(p=="イエッサン♂")||(p=="イエッサン♀")){
 		visi(2, "♂", "♀");
-	}else if((p=="パフュートン♂")||(p=="パフュートン♀")){
+	}else if((p=="パフュートン")||(p=="パフュートン♂")||(p=="パフュートン♀")){
 		visi(2, "♂", "♀");
-	}else if((p=="イルカマン(ナイーブ)")||(p=="イルカマン(マイティ)")){
+	}else if((p=="イルカマン")||(p=="イルカマン(ナイーブ)")||(p=="イルカマン(マイティ)")){
 		visi(2, "ナイーブ", "マイティ");
 	}else{
 		visi(0);
@@ -25,6 +27,12 @@ function mega_button(){
 function visi(num, var1, var2, var3, var4){
 	if(num == 0){
 		document.nForm.elements['mega0'].style.visibility="hidden";
+		document.nForm.elements['mega1'].style.visibility="hidden";
+		document.nForm.elements['mega2'].style.visibility="hidden";
+		document.nForm.elements['mega3'].style.visibility="hidden";
+	}else if(num == 1){
+		document.nForm.elements['mega0'].value = var1;
+		document.nForm.elements['mega0'].style.visibility="visible";
 		document.nForm.elements['mega1'].style.visibility="hidden";
 		document.nForm.elements['mega2'].style.visibility="hidden";
 		document.nForm.elements['mega3'].style.visibility="hidden";
@@ -58,7 +66,7 @@ function visi(num, var1, var2, var3, var4){
 //メガシンカボタン
 function megachange(num){
 	var p = document.nForm.elements['pokename'].value;
-		if((p=="ニャース(通常)")||(p=="ニャース(ガラル)")){
+		if((p=="ニャース")||(p=="ニャース(通常)")||(p=="ニャース(ガラル)")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(ガラル\)/g,"");
  			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(通常\)/g,"");
@@ -68,7 +76,7 @@ function megachange(num){
  			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(通常\)/g,"");
  			document.nForm.elements['pokename'].value= "" + document.nForm.elements['pokename'].value + "(ガラル)";
  		}
-	}else if((p=="ウパー(通常)")||(p=="ウパー(パルデア)")){
+	}else if((p=="ウパー")||(p=="ウパー(通常)")||(p=="ウパー(パルデア)")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(パルデア\)/g,"");
  			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(通常\)/g,"");
@@ -78,13 +86,19 @@ function megachange(num){
  			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(通常\)/g,"");
  			document.nForm.elements['pokename'].value= "" + document.nForm.elements['pokename'].value + "(パルデア)";
  		}
- 	}else if((p=="ロトム(基本)")||(p=="ロトム(FC)")){
+ 	}else if((p=="ケンタロス")||(p=="ケンタロス(パルデア)")){
+		if(parseInt(num)==0){
+ 			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(パルデア\)/g,"");
+ 			document.nForm.elements['pokename'].value = document.nForm.elements['pokename'].value.replace(/\(通常\)/g,"");
+ 			document.nForm.elements['pokename'].value= "" + document.nForm.elements['pokename'].value + "(パルデア)";
+ 		}
+ 	}else if((p=="ロトム")||(p=="ロトム(基本)")||(p=="ロトム(FC)")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value="ロトム(基本)";
  		}else if(parseInt(num)==1){
  			document.nForm.elements['pokename'].value="ロトム(FC)";
  		}
-	}else if((p=="ルガルガン(真昼)")||(p=="ルガルガン(真夜中)")||(p=="ルガルガン(黄昏)")){
+	}else if((p=="ルガルガン")||(p=="ルガルガン(真昼)")||(p=="ルガルガン(真夜中)")||(p=="ルガルガン(黄昏)")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value="ルガルガン(真昼)";
  		}else if(parseInt(num)==1){
@@ -92,25 +106,25 @@ function megachange(num){
  		}else if(parseInt(num)==2){
  			document.nForm.elements['pokename'].value="ルガルガン(黄昏)";
  		}
-	}else if((p=="コオリッポ(アイス)")||(p=="コオリッポ(ナイス)")){
+	}else if((p=="コオリッポ")||(p=="コオリッポ(アイス)")||(p=="コオリッポ(ナイス)")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value="コオリッポ(アイス)";
  		}else if(parseInt(num)==1){
  			document.nForm.elements['pokename'].value="コオリッポ(ナイス)";
  		}
-	}else if((p=="イエッサン♂")||(p=="イエッサン♀")){
+	}else if((p=="イエッサン")||(p=="イエッサン♂")||(p=="イエッサン♀")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value="イエッサン♂";
  		}else if(parseInt(num)==1){
  			document.nForm.elements['pokename'].value="イエッサン♀";
  		}
- 	}else if((p=="パフュートン♂")||(p=="パフュートン♀")){
+ 	}else if((p=="パフュートン")||(p=="パフュートン♂")||(p=="パフュートン♀")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value="イエッサン♂";
  		}else if(parseInt(num)==1){
  			document.nForm.elements['pokename'].value="イエッサン♀";
  		}
- 	}else if((p=="イルカマン(ナイーブ)")||(p=="イルカマン(マイティ)")){
+ 	}else if((p=="イルカマン")||(p=="イルカマン(ナイーブ)")||(p=="イルカマン(マイティ)")){
 		if(parseInt(num)==0){
  			document.nForm.elements['pokename'].value="イルカマン(ナイーブ)";
  		}else if(parseInt(num)==1){
